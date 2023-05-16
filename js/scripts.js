6e50b10b9ce91e9203f48e045fee67d9e7c02bcc
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-05-16 12:19:38 +0200

    used IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,38 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-    abilities: ["chlorophyll", "overgrow"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire"],
-    abilities: ["blaze", "solar-power"],
-  },
-  {
-    name: "Rattata",
-    height: 0.3,
-    types: ["normal"],
-    abilities: ["run-away", "hustle", "guts"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+      abilities: ["chlorophyll", "overgrow"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["fire"],
+      abilities: ["blaze", "solar-power"],
+    },
+    {
+      name: "Rattata",
+      height: 0.3,
+      types: ["normal"],
+      abilities: ["run-away", "hustle", "guts"],
+    },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return { getAll, add };
+})();
 
 // // with template literals
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   let pokemonHeightCheck =
     pokemon.height > 0.6
       ? " <span class='highlight'>- Wow, that's big!</span>"
